b70da2f169e8b6fa40e51fb447bd4bc2c88597c4
Vinícius Khouri Salmeron <viniksalmeron2910@gmail.com>
2026-06-01 15:57:16 -0300

    Modal para adicionar pedidos

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,4 @@
+//Código para data 
 const dataAtual = document.getElementById("dataAtual");
 
 const hoje = new Date();
@@ -8,3 +9,26 @@ dataAtual.innerText = hoje.toLocaleDateString("pt-BR", {
     month: "long",
     day: "numeric"
 });
+
+//Cógido do popup (modal) de adicionar pedidos
+/*
+const modal = document.getElementById('modalAdicionar');
+        const btnAbrir = document.getElementById('abrirModal');
+        const btnFechar = document.getElementById('fecharModal');
+
+        // Abre o modal ao clicar no botão de abertura
+        btnAbrir.addEventListener('click', () => {
+            modal.showModal();
+        });
+
+        // Fecha o modal ao clicar no botão de fechamento
+        btnFechar.addEventListener('click', () => {
+            modal.close();
+        });
+
+        // Opcional: Fecha o modal se o usuário clicar no fundo escuro (fora do modal)
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.close();
+            }
+        });*/
